dffe2149ad5805ef642a16e0724f3cfe8b133c74
Anumart Chaichana <tonanumart@gmail.com>
2016-09-23 15:59:18 +0700

    Example2

--- LINQExample/Exams/FindOdds.cs
+++ LINQExample/Exams/FindOdds.cs
@@ -6,11 +6,31 @@ using System.Threading.Tasks;
 
 namespace LINQExample.Exams
 {
-    public class FindOdds
+    public class ExampleLinqTest
     {
         public static int[] WhereIsOddsNumbers(int[] array) {
-
+            throw new NotImplementedException();
             return null;
+        }
+
+        public static string EvenLengthString(string[] mString)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static decimal GetSumOfOrders(List<Model.Order> orders)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static List<Model.Order> GetOrdersPriceMoreThanOrEqual20(List<Model.Order> orders)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal static int CountAeiou(string longString)
+        {   //ignore case A = a
+            throw new NotImplementedException();
         }
     }
 }
--- LINQExample/TestExample.cs
+++ LINQExample/TestExample.cs
@@ -1,4 +1,5 @@
 ﻿using LINQExample.Exams;
+using LINQExample.Model;
 using NUnit.Framework;
 using System;
 using System.Collections.Generic;
@@ -8,16 +9,59 @@ using System.Threading.Tasks;
 
 namespace LINQExample
 {
-    
+
     public class TestExample
     {
         [Test]
-        public void Test_LINQ_1() {
+        public void Test_LINQ_Where()
+        {
             var testArray = new int[] { 1, 2, 3, 4, 5, 6, 7, 8 };
-            var result = new int[]{1,3,5,7};
-            Assert.AreEqual(result, FindOdds.WhereIsOddsNumbers(testArray));
-            
+            var result = new int[] { 1, 3, 5, 7 };
+            Assert.AreEqual(result, ExampleLinqTest.WhereIsOddsNumbers(testArray));
         }
+
+        [Test]
+        public void Test_LINQ_Find_EvenLengthString()
+        {
+            var mString = new string[] { "anumart", "siwat", "dataeven" };
+            Assert.AreEqual("dataeven", ExampleLinqTest.EvenLengthString(mString));
+        }
+
+        [Test]
+        public void FindSumOfOrdersThis() {
+            var orders = new List<Order>();
+            orders.Add(new Order(){ Price = 50m });
+            orders.Add(new Order() { Price = 18m });
+            orders.Add(new Order() { Price = 20m });
+            orders.Add(new Order() { Price = 7.5m });
+            Assert.AreEqual(95.5, ExampleLinqTest.GetSumOfOrders(orders));
+        }
+
+        [Test]
+        public void FindPriceMoreThan20()
+        {
+            var orders = new List<Order>();
+            var price50 = new Order() { Price = 50m };
+            orders.Add(price50);
+            orders.Add(new Order() { Price = 10m });
+            var price30 = new Order() { Price = 30m };
+            orders.Add(price30);
+            orders.Add(new Order() { Price = 1.5m });
+
+            var result = new List<Order>();
+            result.Add(price50);
+            result.Add(price30);
+            Assert.AreEqual(result, ExampleLinqTest.GetOrdersPriceMoreThanOrEqual20(orders));
+        }
+
+
+        [Test]
+        public void FindAEIOU()
+        {
+            string longString = "asjdgqyweu1237865weasydgajet32ayusdhqii132uijasdhkjcetewfy1523rysada9dasiodjqldlwek,goepripas[ldzjxcnvzhxdfqtywieuty231546";
+            Assert.AreEqual(29, ExampleLinqTest.CountAeiou(longString));
+        }
+
 
     }
 }
