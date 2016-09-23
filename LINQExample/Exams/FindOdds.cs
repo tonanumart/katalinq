74c0a1f3f248ddee1a75b7b75783abd998185349
kwanchanokz <Fainarakz@gmail.com>
2016-09-23 17:02:08 +0700

    test commit

---
+++ LINQExample/Exams/FindOdds.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQExample.Exams
+{
+    public class ExampleLinqTest
+    {
+        public static int[] WhereIsOddsNumbers(int[] array) 
+        {
+            var oddsNumber = array.Where(value => value % 2 == 1).ToArray();
+            return oddsNumber;
+        }
+
+        public static string EvenLengthString(string[] mString)
+        {
+            string evenLength = mString.Where(value => value.Length % 2 == 0).First();
+            return evenLength;
+        }
+
+        public static decimal GetSumOfOrders(List<Model.Order> orders)
+        {
+            //decimal sum = orders.
+
+            throw new NotImplementedException();
+        }
+
+        public static List<Model.Order> GetOrdersPriceMoreThanOrEqual20(List<Model.Order> orders)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static int CountAeiou(string longString)
+        {   //ignore case A = a
+            //int count = longString.Where(str => str.subString();
+            throw new NotImplementedException();
+        }
+
+        public static int[] Mod7AndPlus1(int[] series)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
