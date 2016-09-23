16b7e9db057c0ad09bec54325971c9ff4a633a6f
Anumart Chaichana <tonanumart@gmail.com>
2016-09-23 15:38:44 +0700

    First Test

---
+++ LINQExample/Exams/FindOdds.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQExample.Exams
+{
+    public class FindOdds
+    {
+        public static int[] WhereIsOddsNumbers(int[] array) {
+
+            return null;
+        }
+    }
+}
---
+++ LINQExample/TestExample.cs
@@ -0,0 +1,23 @@
+﻿using LINQExample.Exams;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQExample
+{
+    
+    public class TestExample
+    {
+        [Test]
+        public void Test_LINQ_1() {
+            var testArray = new int[] { 1, 2, 3, 4, 5, 6, 7, 8 };
+            var result = new int[]{1,3,5,7};
+            Assert.AreEqual(result, FindOdds.WhereIsOddsNumbers(testArray));
+            
+        }
+
+    }
+}
